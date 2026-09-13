fd9682477282f09c5b12129e1b187c780ef4367a
Mohammad Amin Ahmadi <pmreza0831@gmail.com>
2026-09-13 13:50:37 +0330

    Replace advanced but broken code with working customtkinter version

--- fix2.py
+++
@@ -1,4 +0,0 @@
-with open('main.py', 'r', encoding='utf-8') as f: text = f.read()
-text = text.replace('ft.icons.', 'ft.Icons.')
-with open('main.py', 'w', encoding='utf-8') as f: f.write(text)
-print('Fixed Flet icons API changes!')
